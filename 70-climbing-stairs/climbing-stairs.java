73e6d33d5da27111656cc392dbcc8b565a5a3a85
VAISHNAVI KONKEPUDI <konkepudivaishnavi3@gmail.com>
2026-08-11 22:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (97.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,15 +2,15 @@ class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
 
-        int p1 = 2; 
-        int p2 = 1; 
+        int pi1 = 2; 
+        int pi2 = 1; 
 
-        for (int i = 3; i <= n; i++) {
-            int cur = p1 + p2;
-            p2 = p1;
-            p1 = cur;
+        for (int i = 3; i <=n; i++) {
+            int cur = pi1 + pi2;
+            pi2 = pi1;
+            pi1 = cur;
         }
 
-        return p1;
+        return pi1;
     }
 }
